4fc9f3cbec8c47b7021df93927ae23801ab6430e
Jozek Hajduk <hajduksanchez.dev@gmail.com>
2022-06-30 11:22:22 -0500

    🚧 [ADD] Movie budget

--- src/components/movieDetails/MovieDetails.style.ts
+++ src/components/movieDetails/MovieDetails.style.ts
@@ -3,6 +3,7 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     marginTop: 20,
+    marginBottom: 40,
   },
   informationContainer: {
     marginHorizontal: 20,
@@ -17,7 +18,19 @@ export const styles = StyleSheet.create({
     paddingTop: 5,
   },
   genders: {
+    paddingBottom: 10,
     fontSize: 14,
     opacity: 0.6,
+  },
+  budget: {
+    paddingTop: 10,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  text: {
+    paddingVertical: 5,
+    fontSize: 16,
   },
 });
--- src/components/movieDetails/MovieDetails.tsx
+++ src/components/movieDetails/MovieDetails.tsx
@@ -7,6 +7,7 @@ import { Cast } from '../../common/interface/credits.interface';
 import { MovieDetail } from '../../common/interface/details.interface';
 import { styles } from './MovieDetails.style';
 import { setStarIconByRaiting } from '../../common/helpers/stars/stars.helper';
+import { API_RESPONSE_LANGUAGE } from '@env';
 
 interface MovieDetailsProps {
   movieData: MovieDetail;
@@ -21,10 +22,19 @@ const MovieDetails = ({ movieData, cast }: MovieDetailsProps) => {
           {setStarIconByRaiting(movieData.vote_average).map(({ type, index }) => (
             <Icon name={type} size={20} style={styles.star} key={index} />
           ))}
-          <Text style={styles.date}> {movieData.vote_average}</Text>
+          <Text style={styles.date}> {movieData.vote_average / 2}</Text>
         </View>
         <Text style={styles.date}>{movieData.release_date}</Text>
         <Text style={styles.genders}>{movieData.genres.map(gender => gender.name).join(', ')}</Text>
+        <Text style={styles.text}>{movieData.overview}</Text>
+        <View style={styles.budget}>
+          <Text style={styles.title}>Budget</Text>
+          <Text style={styles.text}>
+            {new Intl.NumberFormat(API_RESPONSE_LANGUAGE, { style: 'currency', currency: 'USD' }).format(
+              movieData.budget,
+            )}
+          </Text>
+        </View>
       </View>
     </View>
   );
